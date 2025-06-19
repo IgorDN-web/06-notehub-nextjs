6db0e0240351f49b8f1008d217bc9cbaa4e676fc
IgorD <avtosklokyiv@gmail.com>
2025-06-19 22:22:57 +0300

    fgt

--- types/note.ts
+++ types/note.ts
@@ -1,7 +1,12 @@
 export interface Note {
-    id: number;
-    title: string;
-    content: string;
-    tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
-    isArchived: boolean;
-  }
+  id: number;
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export interface CreateNoteValues {
+  title: string;
+  content?: string;
+  tag: "Work" | "Personal" | "Meeting" | "Shopping" | "Todo";
+}
