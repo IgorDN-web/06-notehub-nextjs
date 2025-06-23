e96a9442547c5fc3d13c2da882d4e2bcefd71004
IgorD <avtosklokyiv@gmail.com>
2025-06-23 19:39:50 +0300

    fsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { fetchNoteId } from '@/lib/api';
+import { fetchNoteById } from '@/lib/api';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import NoteDetailsClient from './NoteDetails.client';
 
@@ -10,7 +10,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
 
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
-    queryFn: () => fetchNoteId(String(id)),
+    queryFn: () => fetchNoteById(String(id)),
   });
 
   return (
